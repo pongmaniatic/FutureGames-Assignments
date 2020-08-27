8f9ca5cfae4d21cb7db5fe28460bb4aa1d71bbec
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2020-08-27 17:59:44 +0200

    advanced in the assignment

--- Assets/Script/AssignmentTwo.cs
+++ Assets/Script/AssignmentTwo.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class AssignmentTwo : MonoBehaviour
 {
+    // Problem 1 counteract gravity
+    private Rigidbody RigidBody;
 
-    public Vector3 PositionCenterOfMass;
-    public Rigidbody rb;
-
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        PositionCenterOfMass = rb.centerOfMass;
-        Debug.Log(" centro de masa: "+ PositionCenterOfMass);
+        // Problem 1 counteract gravity
+        RigidBody = GetComponent<Rigidbody>(); // get the current objects rigidbody component, that component is incharge of that objects gravity.
     }
 
+    
 
+    private void FixedUpdate()// for the longest time I did not realize why it was over correcting, it was because the normal update mas applying the counter force more times than normal gravity.
+    {
+        RigidBody.AddForce( -Physics.gravity, ForceMode.Acceleration); // this counters the gravity by applying an equal but opposite Aceleration to it, acceleration is not affected by mass in unity so you can change its mass and it counter acts gravity all the same
+        RigidBody.velocity = new Vector3(0, 0, 0); // this resets the velocity, in case the object is already falling by the time this script is activated.
+    }
 }
---
+++ Assets/Script/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : ScriptableObject
+
+{
+    private int health = 3;
+
+    private void FixedUpdate()
+    {
+
+        if (health <= 0)
+        {
+            Destroy(this, 5);
+        }
+    }
+
+    void HitByRay()
+    {
+        health -= 1;
+    }
+}

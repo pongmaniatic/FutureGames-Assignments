810e79242cedf7f1b700654e664db76e346b9638
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2020-08-27 10:24:38 +0200

    assignments

---
+++ Assets/Script/AssignmentTwo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignmentTwo : MonoBehaviour
+{
+
+    public Vector3 PositionCenterOfMass;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        PositionCenterOfMass = rb.centerOfMass;
+        Debug.Log(" centro de masa: "+ PositionCenterOfMass);
+    }
+
+
+}
